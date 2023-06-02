8c090748d465d7917dfceebfaeb4992562c8eac8
Dimas Ichsanul Arifin <dimas.ichsanul@ui.ac.id>
2023-06-02 21:12:13 +0700

    fix: Change Time Unit for inferenceTime to Miliseconds (3)

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def predict(item:Item):
 
     predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
     confidence = np.max(prediction[0])
-    inference_time = (end_time - start_time)  * 10**(-3)
+    inference_time = (end_time - start_time)
 
     return {
         'filename': item.filename,
